d76009463f257eb7a7f4ca8b387cddb4697031d9
dinhdung710004 <166023375+LettttttDuelll@users.noreply.github.com>
2025-07-16 13:57:44 +0700

    Update Cash_register.js

--- Cash_register.js
+++ Cash_register.js
@@ -1,7 +1,9 @@
 ** start of script.js **
 /*
 Cash Register
-Design a cash register drawer function checkCashRegister() that accepts purchase price as the first argument (price), payment as the second argument (cash), and cash-in-drawer (cid) as the third argument.
+Design a cash register drawer function checkCashRegister() that accepts purchase price as the first argument (price), payment as the second argument (cash),
+
+and cash-in-drawer (cid) as the third argument.
 
 cid is a 2D array listing available currency.
 
